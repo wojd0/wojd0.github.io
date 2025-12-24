3d611633141414431282f27e3161928bb0707e55
Wojciech Duda <wojciechduda03@gmail.com>
2025-12-24 21:46:50 +0100

    build: update deps

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,28 @@
-import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import {
+  enableProdMode,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { environment } from './environments/environment';
 import {
+  HttpClient,
   provideHttpClient,
   withInterceptorsFromDi,
-  HttpClient,
 } from '@angular/common/http';
 import { NgOptimizedImage } from '@angular/common';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  TranslateModule,
+  TranslateLoader,
+  provideTranslateService,
+} from '@ngx-translate/core';
+import {
+  TranslateHttpLoader,
+  provideTranslateHttpLoader,
+} from '@ngx-translate/http-loader';
 import { AppComponent } from './app/app.component';
 import { provideAppRoutes } from './routes';
-
-function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 if (environment.production) {
   enableProdMode();
@@ -23,20 +30,15 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZoneChangeDetection(),importProvidersFrom(
-      BrowserModule,
-      NgOptimizedImage,
-      TranslateModule.forRoot({
-        defaultLanguage: 'en',
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      })
-    ),
+    provideZoneChangeDetection(),
+    importProvidersFrom(BrowserModule, NgOptimizedImage),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     provideAppRoutes(),
+    provideTranslateHttpLoader(),
+    provideTranslateService({
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader(),
+    }),
   ],
 }).catch((err) => console.error(err));
